078cdd910fe2c48414783f16b5c51ced9b71c6ca
Christopher Brown <christopher.p.brown@gmail.com>
2015-11-18 14:03:07 -0700

    working on DOM

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
 // alert('yow!');
+var myButton = document.getElementsByTagName('button')[0];
+var myTarget = document.getElementsByTagName('section')[0];
+console.log(myButton);
+console.log(myTarget);
+
+myButton.addEventListener('click', function(){
+  var myAmount = myButon.value;
+  var tipAmount = 1.2;
+  alert(myAmount);
+  myTarget.text = "hi hi hi hi";
+}, false);
