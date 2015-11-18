1dcdf80a09fb0444851fe8aab73cf8a7f29c3dfd
Christopher Brown <christopher.p.brown@gmail.com>
2015-11-18 16:22:58 -0700

    scale

--- js/app.js
+++ js/app.js
@@ -2,19 +2,30 @@
 var myButton = document.getElementsByTagName('button')[0];
 var myTarget = document.getElementsByTagName('section')[0];
 var myInput = document.getElementsByTagName('input')[0];
+var myScale = document.getElementsByName('cheap');
+
 console.log(myButton);
 console.log(myTarget);
 
 myButton.addEventListener("click", function(event){
   var myAmount = myInput.value * 1;
   if (isValidInput(myAmount)) {
-    var tipAmount = 0.2;
+    var tipAmount = getTipAmount();
     myTarget.innerText = 'You should tip $' + (myAmount * tipAmount).toFixed(2);
   } else {
     myTarget.innerText = "Enter a valid number."
   }
   event.preventDefault();
 });
+
+function getTipAmount(){
+  for (var x = 0; x < myScale.length; x++){
+    if(myScale[x].checked){
+      return myScale[x].value * 1;
+    }
+  }
+  return 0.2;
+}
 
 function isValidInput(i) {
   console.log(i + ': ' + typeof(i));
