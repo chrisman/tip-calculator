cceaeda0662e6d603bb09b954398f3b8b3918f17
Christopher Brown <christopher.p.brown@gmail.com>
2015-11-18 15:58:56 -0700

    functioning

--- js/app.js
+++ js/app.js
@@ -1,12 +1,22 @@
 // alert('yow!');
 var myButton = document.getElementsByTagName('button')[0];
 var myTarget = document.getElementsByTagName('section')[0];
+var myInput = document.getElementsByTagName('input')[0];
 console.log(myButton);
 console.log(myTarget);
 
-myButton.addEventListener('click', function(){
-  var myAmount = myButon.value;
-  var tipAmount = 1.2;
-  alert(myAmount);
-  myTarget.text = "hi hi hi hi";
-}, false);
+myButton.addEventListener("click", function(event){
+  var myAmount = myInput.value * 1;
+  if (isValidInput(myAmount)) {
+    var tipAmount = 0.2;
+    myTarget.innerText = 'You should tip $' + (myAmount * tipAmount).toFixed(2);
+  } else {
+    myTarget.innerText = "Enter a valid number."
+  }
+  event.preventDefault();
+});
+
+function isValidInput(i) {
+  console.log(i + ': ' + typeof(i));
+  return (typeof(i) === 'number') ? true : false;
+}
